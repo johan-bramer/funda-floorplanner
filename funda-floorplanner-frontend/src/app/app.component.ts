6d0ad460e8cd27552a2992d08fbcadbe400eda57
Johan Bramer <johan.bramer@belsimpel.nl>
2025-10-07 22:47:11 +0200

    toglle

--- funda-floorplanner-frontend/src/app/app.component.ts
+++ funda-floorplanner-frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgOptimizedImage } from "@angular/common";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
