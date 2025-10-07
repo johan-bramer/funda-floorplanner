ef4ea6b3c17102042f19d3c6b93efedbf1bd5a84
Johan Bramer <johan.bramer@belsimpel.nl>
2025-10-07 23:15:04 +0200

    some setup

--- funda-floorplanner-frontend/src/app/app.component.ts
+++ funda-floorplanner-frontend/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgOptimizedImage } from "@angular/common";
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, MatCardModule, MatButtonModule, MatInputModule, MatProgressBarModule, MatIconModule, MatListModule, MatTooltipModule, MatSnackBarModule, CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
